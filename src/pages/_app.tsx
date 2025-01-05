9fd244d572f989b2de38e9478242928f0a28baa1
Fabrício Santos <fabriciojlsantos@gmail.com>
2025-01-05 19:55:53 -0300

    add page template and toast container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
+import Template from "@/template";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Template>
+    <Component {...pageProps} />
+    <ToastContainer />
+    </Template>
+  )
 }
